b21100718c96f63f5b0490d7b0270315f05d3428
Luke7 <lucaspy1943@icloud.com>
2024-12-11 19:18:45 +0100

    diseño e imagenes agregadas

--- src/PortfolioApp.jsx
+++ src/PortfolioApp.jsx
@@ -1,9 +1,24 @@
+import './index.css';
+
 export function PortfolioApp() {
+	// Lista de rutas de imágenes
+	const images = [
+		"/src/assets/images/image1.jpg",
+		"/src/assets/images/image2.jpg",
+		"/src/assets/images/image3.jpg",
+		"/src/assets/images/image4.jpg",
+		"/src/assets/images/image5.jpg",
+		"/src/assets/images/image6.jpg",
+		"/src/assets/images/image7.jpg",
+	];
 
-  return (
-    <>
-      <h1>Portfolio App</h1>
-    </>
-  )
+	return (
+    <div className="gallery">
+    {images.map((src, index) => (
+      <div className="image-container" key={index}>
+        <img src={src} alt={`Image ${index + 1}`} />
+      </div>
+    ))}
+  </div>
+	);
 }
-
