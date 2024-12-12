1688a4275926a00af1573ecf181f2be8b587bde3
Luke7 <lucaspy1943@icloud.com>
2024-12-12 21:50:52 +0100

    agregado el lazy load y target blank

--- src/PortfolioApp.jsx
+++ src/PortfolioApp.jsx
@@ -2,8 +2,14 @@ import { CiMail } from "react-icons/ci";
 import { FaWhatsapp } from "react-icons/fa"; //
 // Importamos los íconos de FontAwesome
 import "./index.css";
+import { useState } from "react";
 
 export function PortfolioApp() {
+	const [loaded, setLoaded] = useState(false);
+
+	const handleImageLoad = () => {
+		setLoaded(true); // Se activa cuando la imagen se ha cargado
+	};
 	// Lista de rutas de imágenes
 	const images = [
 		"/src/assets/images/image(1).jpg",
@@ -25,19 +31,32 @@ export function PortfolioApp() {
 					<div className={`image-container imgs-port${index}`} key={index}>
 						<img
 							src={src}
-							alt={`Imagen destacada ${index + 1} del portafolio`}
+							alt={`Imagen destacada ${index + 1}`}
+							loading="lazy"
+							onLoad={handleImageLoad} // Activar la animación al cargar
+							className={loaded ? "loaded" : ""}
 						/>
 
 						{index === 9 && (
 							<div className="image-description">
 								{/* WhatsApp icon with link */}
-								<a href="https://wa.me/34612345678" className="contact-link">
-									<FaWhatsapp size={2} /> 641048149
+								<a
+									href="https://wa.me/+34641048149"
+									className="contact-link"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									<FaWhatsapp size={2} /> +34 641 048 149
 								</a>
 								<br />
 								{/* Mail icon with link */}
-								<a href="mailto:correo@ejemplo.com" className="contact-link">
-									<CiMail size={28}  strokeWidth={1} /> pacheco_bianca@icloud.com
+								<a
+									href="mailto:pacheco_bianca@icloud.com"
+									className="contact-link"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									<CiMail size={28} strokeWidth={1} /> pacheco_bianca@icloud.com
 								</a>
 							</div>
 						)}
