72473dbccf1506f9b72d47744217e1e3e7c6f82d
Luke7 <lucaspy1943@icloud.com>
2024-12-12 20:04:03 +0100

    Diseño terminado, queda agregar los enlaces

--- src/PortfolioApp.jsx
+++ src/PortfolioApp.jsx
@@ -1,28 +1,34 @@
 import './index.css';
 
 export function PortfolioApp() {
-	// Lista de rutas de imágenes
-	const images = [
-		"/src/assets/images/image(1).jpg",
-		"/src/assets/images/image(2).jpg",
-		"/src/assets/images/image(3).jpg",
-		"/src/assets/images/image(4).jpg",
-		"/src/assets/images/image(5).jpg",
-		"/src/assets/images/image(6).jpg",
-		"/src/assets/images/image(7).jpg",
+  // Lista de rutas de imágenes
+  const images = [
+    "/src/assets/images/image(1).jpg",
+    "/src/assets/images/image(2).jpg",
+    "/src/assets/images/image(3).jpg",
+    "/src/assets/images/image(4).jpg",
+    "/src/assets/images/image(5).jpg",
+    "/src/assets/images/image(6).jpg",
+    "/src/assets/images/image(7).jpg",
     "/src/assets/images/image(8).jpg",
     "/src/assets/images/image(9).jpg",
     "/src/assets/images/image(10).jpg",
+  ];
 
-	];
+  return (
+    <main className="cont">
+      <section className="gallery">
+        {images.map((src, index) => (
+          <div className={`image-container imgs-port${index}`} key={index}>
+            <img 
+              src={src} 
+              alt={`Imagen destacada ${index + 1} del portafolio`} 
+            />
 
-	return (
-    <div className="gallery">
-    {images.map((src, index) => (
-      <div className="image-container" key={index}>
-        <img src={src} alt={`Image ${index + 1}`} />
-      </div>
-    ))}
-  </div>
-	);
+            
+          </div>
+        ))}
+      </section>
+    </main>
+  );
 }
